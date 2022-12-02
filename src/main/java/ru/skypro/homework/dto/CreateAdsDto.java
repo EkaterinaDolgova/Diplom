10c35d5573a720bad0b1d012c42ace7c340a5a67
Евгений <ugend84@yandex.ru>
2022-12-02 22:03:24 +0300

    Движемся к концу

--- src/main/java/ru/skypro/homework/dto/CreateAdsDto.java
+++ src/main/java/ru/skypro/homework/dto/CreateAdsDto.java
@@ -1,6 +1,7 @@
 package ru.skypro.homework.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
 import org.springframework.validation.annotation.Validated;
 
 /**
@@ -9,47 +10,9 @@ import org.springframework.validation.annotation.Validated;
 @Validated
 @javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.SpringCodegen", date = "2022-10-29T09:53:39.743Z[GMT]")
 
-
+@Data
 public class CreateAdsDto {
-    @JsonProperty()
     private String description;
-
-    @JsonProperty()
     private Integer price;
-
-    @JsonProperty()
     private String title;
-
-    public CreateAdsDto(String description, Integer price, String title) {
-        this.description = description;
-        this.price = price;
-        this.title = title;
-    }
-
-    public CreateAdsDto() {
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public void setPrice(Integer price) {
-        this.price = price;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public Integer getPrice() {
-        return price;
-    }
-
-    public String getTitle() {
-        return title;
-    }
 }
